986d228340e68c63fd1ef28d34e0aa8598f505e3
b8b <25864173+b8b@users.noreply.github.com>
2019-05-09 13:46:22 +0200

    upgrade to kotlin-1.3.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.io.*
 import java.security.MessageDigest
 
 plugins {
-    val kotlinVersion = "1.3.30"
+    val kotlinVersion = "1.3.31"
 
     kotlin("jvm") version kotlinVersion
     id("org.jetbrains.dokka") version "0.9.17"
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "org.cikit.kotlin_script"
-version = "1.3.30.0"
+version = "1.3.31.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
