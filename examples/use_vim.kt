b971e5c5e37cc38a13c1e9dbfa03dac55d7c0ea3
b8b <25864173+b8b@users.noreply.github.com>
2019-06-06 15:05:56 +0200

    upgrade embedded installer

--- examples/use_vim.kt
+++ examples/use_vim.kt
@@ -10,19 +10,19 @@
 #   |_|\_\___/ \__|_|_|_| |_| |___/\___|_|  |_| .__/ \__|
 #                         ______              | |
 #                        |______|             |_|
-v=1.3.30.0
+v=1.3.31.1
 artifact=org/cikit/kotlin_script/kotlin_script/"$v"/kotlin_script-"$v".sh
+repo=${repo:-https://repo1.maven.org/maven2}
 if ! [ -e "${local_repo:=$HOME/.m2/repository}"/"$artifact" ]; then
   fetch_s="$(command -v fetch) -aAqo" || fetch_s="$(command -v curl) -fSso"
   mkdir -p "$local_repo"/org/cikit/kotlin_script/kotlin_script/"$v"
   tmp_f="$(mktemp "$local_repo"/"$artifact"~XXXXXXXXXXXXXXXX)" || exit 1
-  if ! ${fetch_cmd:="$fetch_s"} "$tmp_f" \
-      "${repo:=https://repo1.maven.org/maven2}"/"$artifact"; then
+  if ! ${fetch_cmd:="$fetch_s"} "$tmp_f" "$repo"/"$artifact"; then
     echo "error: failed to fetch kotlin_script" >&2
     rm -f "$tmp_f"; exit 1
   fi
   case "$(openssl dgst -sha256 -r < "$tmp_f")" in
-  "82dfd24da8e2cb725a548c9ef294fdf1ac5b5a6d209d9c00c42a241e7017e587 "*)
+  "d6cd4372ee10a2b2ff0db496a376be56823f0b7a6f36ccd9aaf8b9e7a26089b0 "*)
     mv -f "$tmp_f" "$local_repo"/"$artifact" ;;
   *)
     echo "error: failed to validate kotlin_script" >&2
